cfbd6d37ac41fe1bf11df4d89152f8a7075958f7
Swangi Kumari <85639103+swangi-kumari@users.noreply.github.com>
2022-10-28 18:15:37 +0530

    Create 110A-A. Nearly Lucky Number.java

---
+++ 800 Rated/110A-A. Nearly Lucky Number.java
@@ -0,0 +1,56 @@
+/**
+110A. Nearly Lucky Number
+
+Petya loves lucky numbers. We all know that lucky numbers are the positive integers whose decimal representations contain only the lucky digits 4 and 7. For example, numbers 47, 744, 4 are lucky and 5, 17, 467 are not.
+
+Unfortunately, not all numbers are lucky. Petya calls a number nearly lucky if the number of lucky digits in it is a lucky number. He wonders whether number n is a nearly lucky number.
+
+Input
+The only line contains an integer n (1 ≤ n ≤ 1018).
+
+Please do not use the %lld specificator to read or write 64-bit numbers in С++. It is preferred to use the cin, cout streams or the %I64d specificator.
+
+Output
+Print on the single line "YES" if n is a nearly lucky number. Otherwise, print "NO" (without the quotes).
+
+Examples
+inputCopy
+40047
+outputCopy
+NO
+inputCopy
+7747774
+outputCopy
+YES
+inputCopy
+1000000000000000000
+outputCopy
+NO
+**/
+
+import java.util.Scanner;
+ 
+public class A_NearlyLuckyNumber {
+    public static boolean isLucky(long n){
+        return n == 7 || n == 4;
+    }
+ 
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        long n = s.nextLong();
+        int count = 0;
+        while(n>0){
+            long ans = n%10;
+            if(isLucky(ans)){
+                count++;
+            }
+            n = n / 10;
+        }
+        if(isLucky(count)){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
